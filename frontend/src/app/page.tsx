2a411ce3d6149c8b7e6c1173cef077230c1ea276
Damil001 <damiljamil63@gmail.com>
2025-08-18 23:57:03 +0500

    fix: issue resolved

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -76,13 +76,13 @@ export default function Home() {
             marginBottom: "0.25rem",
           }}
         >
-          Welcome. This tool isn‘t about preservation, it’s about preparation.
-          Some archives collect dust. Not us, we collect resistance. This is
-          your playground, now choose your path: Explore the archive's
-          categories, or follow the protest sign. some archives collect dust.
-          not us, we collect resistance. this is your playground, now choose
-          your path: explore the archive&apos;s categories, or follow the
-          protest sign.
+          Welcome. This tool isn&apos;t about preservation, it&apos;s about
+          preparation. Some archives collect dust. Not us, we collect
+          resistance. This is your playground, now choose your path: Explore the
+          archive&apos;s categories, or follow the protest sign. some archives
+          collect dust. not us, we collect resistance. this is your playground,
+          now choose your path: explore the archive&apos;s categories, or follow
+          the protest sign.
         </p>
         <div className="mb-10" />
         <div className="flex flex-col gap-4">
